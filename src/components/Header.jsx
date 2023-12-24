4ca1451ff692ce46fe13d1f921e47c7bb726b080
jeidervanegas <146299580+jeidervanegas@users.noreply.github.com>
2023-12-24 15:02:34 -0500

    first commit3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,12 @@ import { useState } from 'react'
 import { useBookStore } from '../store/bookStore'
 
 export const Header = () => {
-
   const [value, setValue] = useState('cat')
-  const updateValue = useBookStore(state => state.updateValue)
+  const updateValue = useBookStore((state) => state.updateValue)
 
   const handleKey = (e) => {
-    if(e.key === 'Enter') {
-      console.log('Presionaste Enter', value);
+    if (e.key === 'Enter') {
+      console.log('Presionaste Enter', value)
       updateValue(value)
     }
   }
@@ -22,7 +21,9 @@ export const Header = () => {
         <ul className="flex justify-between items-center">
           <div className="flex justify-center items-center gap-1 md:gap-4 font-bold text-gray-700">
             <li>
-              <a href=""><Logo/></a>
+              <a href="">
+                <Logo />
+              </a>
             </li>
             <li className="select">
               <a href="">Inicio</a>
@@ -37,18 +38,35 @@ export const Header = () => {
           <div className="w-full  ">
             {/* <img className='search' src={imageSearch} alt="" /> */}
             <li className=" flex justify-center ">
-        
               <input
                 className="w-[90%]  py-3 px-9 rounded-full bg-gray-200  "
                 placeholder="Buscar imagenes"
                 type="Search"
-                onChange={e=> setValue(e.target.value)}
+                onChange={(e) => setValue(e.target.value)}
                 onKeyDown={handleKey}
               />
             </li>
           </div>
           <li>
-            <a href="">User</a>
+            <a href="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-user-square-rounded"
+                width="40"
+                height="40"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="#597e8d"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 13a3 3 0 1 0 0 -6a3 3 0 0 0 0 6z" />
+                <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" />
+                <path d="M6 20.05v-.05a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v.05" />
+              </svg>
+            </a>
           </li>
         </ul>
       </div>
